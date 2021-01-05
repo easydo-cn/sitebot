715e44e8ed49729d23b55e59704483ad3e668478
黄佳钜 <814926605@qq.com>
2021-01-05 09:23:15 +0000

    Update online_script.py

--- workers/online_script.py
+++ workers/online_script.py
@@ -278,7 +278,7 @@ def online_script(
         level = ''
         if isinstance(progress_level, (str, unicode)):
             level = progress_level.upper()
-        if level:
+        elif level:
             # 遍历level，转成int，用sorted排序，取最低level。对于不存在的level都认为是info。
             level = sorted(
                 map(lambda l: dict(DEBUG=logging.DEBUG, INFO=logging.INFO, ERROR=logging.ERROR).get(l.upper(), 'INFO'),
